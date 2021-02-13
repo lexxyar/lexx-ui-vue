d8450219ad45c1e6135477d3210e54a2d77096d0
Aleksey Komov <lexxyar@ya.ru>
2021-02-13 15:06:22 +0300

    grid local css

--- dev/views/Grids.vue
+++ dev/views/Grids.vue
@@ -1,39 +1,78 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col">
-        col
-      </div>
-      <div class="col">
-        col
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        col
-      </div>
-      <div class="col">
-        col
-      </div>
-      <div class="col">
-        col
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        col
-      </div>
-      <div class="col-6">
-        col-6
-      </div>
-      <div class="col">
-        col
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-8">col-8</div>
-      <div class="col-4">col-4</div>
-    </div>
+<div class="row">
+  <div class="col">
+    col
+  </div>
+  <div class="col">
+    col
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    col
+  </div>
+  <div class="col">
+    col
+  </div>
+  <div class="col">
+    col
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    col
+  </div>
+  <div class="col-6">
+    col-6
+  </div>
+  <div class="col">
+    col
+  </div>
+</div>
+<div class="row">
+  <div class="col-8">col-8</div>
+  <div class="col-4">col-4</div>
+</div>
+
+    <code>
+      <pre>
+&lt;div class=&quot;row&quot;&gt;
+  &lt;div class=&quot;col&quot;&gt;
+    col
+  &lt;/div&gt;
+  &lt;div class=&quot;col&quot;&gt;
+    col
+  &lt;/div&gt;
+&lt;/div&gt;
+&lt;div class=&quot;row&quot;&gt;
+  &lt;div class=&quot;col&quot;&gt;
+    col
+  &lt;/div&gt;
+  &lt;div class=&quot;col&quot;&gt;
+    col
+  &lt;/div&gt;
+  &lt;div class=&quot;col&quot;&gt;
+    col
+  &lt;/div&gt;
+&lt;/div&gt;
+&lt;div class=&quot;row&quot;&gt;
+  &lt;div class=&quot;col&quot;&gt;
+    col
+  &lt;/div&gt;
+  &lt;div class=&quot;col-6&quot;&gt;
+    col-6
+  &lt;/div&gt;
+  &lt;div class=&quot;col&quot;&gt;
+    col
+  &lt;/div&gt;
+&lt;/div&gt;
+&lt;div class=&quot;row&quot;&gt;
+  &lt;div class=&quot;col-8&quot;&gt;col-8&lt;/div&gt;
+  &lt;div class=&quot;col-4&quot;&gt;col-4&lt;/div&gt;
+&lt;/div&gt;
+      </pre>
+    </code>
   </div>
 </template>
 
@@ -45,8 +84,8 @@ export default {
 
 <style scoped>
 *[class^=col] {
-  background-color: gray;
-  border: 1px solid #333333;
+  background-color: #a2a2a2;
+  border: 1px solid #4c4c4c;
   padding: 2rem;
 }
 </style>
