14645605860507e4f87422cc17dadbfd08bdef1a
Aleksey Komov <lexxyar@ya.ru>
2021-02-20 19:00:20 +0300

    gauges fixed
    Signed-off-by: Aleksey Komov <lexxyar@ya.ru>

--- dev/views/Gauges.vue
+++ dev/views/Gauges.vue
@@ -1,38 +1,38 @@
 <template>
   <div>
-  <div class="flex">
-    <lx-gauge v-for="gauge in aGauge" :value="gauge"></lx-gauge>
-  </div>
+    <title-component>Gauges</title-component>
+    <div class="flex">
+      <lx-gauge v-for="gauge in aGauge" :value="gauge"></lx-gauge>
+    </div>
 
-  <code>
     <pre>
-&lt;template&gt;
-  &lt;div class=&quot;flex&quot;&gt;
-    &lt;lx-gauge v-for=&quot;gauge in aGauge&quot; :value=&quot;gauge&quot;&gt;&lt;/lx-gauge&gt;
-  &lt;/div&gt;
-&lt;/template&gt;
-
-&lt;script&gt;
-export default {
-  name: &quot;Gauges&quot;,
-  components:{lxGauge},
-  data() {
-    return ({
-      aGauge: [7, 81, 106, 136, 155]
-    })
-  }
-}
-&lt;/script&gt;
-    </pre>
-  </code>
+<code>&lt;template&gt;</code>
+<code>  &lt;div class=&quot;flex&quot;&gt;</code>
+<code>    &lt;lx-gauge v-for=&quot;gauge in aGauge&quot; :value=&quot;gauge&quot;&gt;&lt;/lx-gauge&gt;</code>
+<code>  &lt;/div&gt;</code>
+<code>&lt;/template&gt;</code>
+<code></code>
+<code>&lt;script&gt;</code>
+<code>export default {</code>
+<code>  name: &quot;Gauges&quot;,</code>
+<code>  components:{lxGauge},</code>
+<code>  data() {</code>
+<code>    return ({</code>
+<code>      aGauge: [7, 81, 106, 136, 155]</code>
+<code>    })</code>
+<code>  }</code>
+<code>}</code>
+<code>&lt;/script&gt;</code></pre>
   </div>
 </template>
 
 <script>
 import {lxGauge} from '@/entry'
+import TitleComponent from "../components/TitleComponent";
+
 export default {
   name: "Gauges",
-  components:{lxGauge},
+  components: {TitleComponent, lxGauge},
   data() {
     return ({
       aGauge: [7, 81, 106, 136, 155]
--- src/lib-components/lxGauge.vue
+++ src/lib-components/lxGauge.vue
@@ -1,34 +1,37 @@
 <template>
-  <div class="gauge">
-    <div class="bar">
-      <div class="gauge-bar" :class="getGaugeBarClass()"
-           :data-value="`${value}%`" data-max="150"></div>
-      <div class="gauge-bar-extra error"
-           :class="getGaugeExtraBarClass()"
-           :data-value="value"></div>
+    <div class="gauge">
+      <div class="bar">
+        <div class="gauge-bar" :class="getGaugeBarClass()"
+             :data-value="`${value}%`" data-max="150"></div>
+        <div class="gauge-bar-extra bg-danger"
+             :class="getGaugeExtraBarClass()"
+             :data-value="value"></div>
+      </div>
+
+      <ul class="ruller">
+        <li :class="`h-${getRullerValuePerc(maxValue, topLimit)} v-${getRullerValuePerc(maxValue)}`">
+          <div>{{ maxValue }}%</div>
+        </li>
+        <li :class="`h-${getRullerValuePerc(topLimit, normalValue)} v-${getRullerValuePerc(topLimit)}`">
+          <div>{{ topLimit }}%</div>
+        </li>
+        <li :class="`h-${getRullerValuePerc(normalValue, bottomLimit)} v-${getRullerValuePerc(normalValue)}`">
+          <div>{{ normalValue }}%</div>
+        </li>
+        <li :class="`h-${getRullerValuePerc(bottomLimit)} v-${getRullerValuePerc(bottomLimit)}`">
+          <div>{{ bottomLimit }}%</div>
+        </li>
+      </ul>
+
     </div>
-
-    <ul class="ruller">
-      <li :class="`h-${getRullerValuePerc(maxValue, topLimit)} v-${getRullerValuePerc(maxValue)}`">
-        <div>{{ maxValue }}%</div>
-      </li>
-      <li :class="`h-${getRullerValuePerc(topLimit, normalValue)} v-${getRullerValuePerc(topLimit)}`">
-        <div>{{ topLimit }}%</div>
-      </li>
-      <li :class="`h-${getRullerValuePerc(normalValue, bottomLimit)} v-${getRullerValuePerc(normalValue)}`">
-        <div>{{ normalValue }}%</div>
-      </li>
-      <li :class="`h-${getRullerValuePerc(bottomLimit)} v-${getRullerValuePerc(bottomLimit)}`">
-        <div>{{ bottomLimit }}%</div>
-      </li>
-    </ul>
-
-  </div>
 </template>
 
 <script>
+import TitleComponent from "../../dev/components/TitleComponent";
+
 export default {
   name: "lxGauge",
+  components: {TitleComponent},
   props: {
     value: {type: Number, default: 0},
   },
@@ -54,18 +57,15 @@ export default {
 
       aCss.push(`h-${nGreenPerc}`)
 
-      let sColor = "success"
+      let sColor = "bg-success"
       if (this.value < 80) {
-        sColor = "error"
+        sColor = "bg-danger"
       } else if (this.value >= 80 && this.value < 100) {
-        sColor = "warning"
+        sColor = "bg-warning"
       } else if (this.value >= 100) {
-        sColor = "success"
+        sColor = "bg-success"
       }
       aCss.push(sColor)
-
-      // let nPerc = Math.round(this.value * 100 / this.maxValue)
-      // aCss.push(`v-${nPerc}`)
 
       return aCss.join(" ")
     },
