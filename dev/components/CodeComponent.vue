a05da94a87b4f864c4a1d008e0095a61e80e6120
Aleksey Komov <lexxyar@ya.ru>
2021-02-20 18:45:42 +0300

    CodeComponent.vue demo component
    Signed-off-by: Aleksey Komov <lexxyar@ya.ru>

---
+++ dev/components/CodeComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="hidden">
+      <slot></slot>
+    </div>
+    <pre class="mt-5">
+{{code}}
+  </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CodeComponent",
+  data(){
+    return ({
+      code:'',
+    })
+  },
+  mounted() {
+    this.code = this.$slots.default[0].text
+    let xstring = '';
+    for(let i = 0; i < this.code.length;i++){
+      xstring += this.code.charCodeAt(i) + ' '
+    }
+    console.log(xstring)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
